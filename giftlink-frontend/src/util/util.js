d7da8ed668bc35d5c6d02b6e8a520fb6f8d5db94
eelnevs <mwl82701@gmail.com>
2024-11-26 15:16:48 -0600

    add details page

--- giftlink-frontend/src/util/util.js
+++ giftlink-frontend/src/util/util.js
@@ -1,6 +1,11 @@
 export const camelToTitleCase = (word) =>
-    [...word]
-        .map((w, i) =>
-            i === 0 ? w.toUpperCase() : w === w.toUpperCase() ? ` ${w}` : w
-        )
-        .join("");
+	[...word]
+		.map((w, i) =>
+			i === 0 ? w.toUpperCase() : w === w.toUpperCase() ? ` ${w}` : w
+		)
+		.join("");
+
+export const formatDate = (timestamp) => {
+	const time = new Date(timestamp * 1000);
+	return time.toLocaleDateString("en-US");
+};
